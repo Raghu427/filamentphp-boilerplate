b8a85ed37180e543ea9f99fa96c6d439d729a774
jairmmz <jaimumiran@gmail.com>
2026-03-04 14:42:41 -0500

    Fix: Correcioń acceso al panel de filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -113,7 +113,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        if ($this->status) {
+        if (!$this->is_active) {
             return false;
         }
 
